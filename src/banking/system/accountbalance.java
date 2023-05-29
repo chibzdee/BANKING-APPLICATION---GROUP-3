8b5c9bea109b8304db704b708e4e2e61fced23f7
DERA0005 <112005422+DERA0005@users.noreply.github.com>
2023-05-29 13:36:26 +0100

    Update accountbalance.java

--- src/banking/system/accountbalance.java
+++ src/banking/system/accountbalance.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
+*/
 package banking.system;
 
 import java.sql.Connection;
