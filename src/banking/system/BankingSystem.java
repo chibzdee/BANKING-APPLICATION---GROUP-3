c1698ae5121db4e8333dbfe1a774741952a4e90e
tonyy09 <134449087+tonyy09@users.noreply.github.com>
2023-05-29 17:58:19 +0100

    Update BankingSystem.java

--- src/banking/system/BankingSystem.java
+++ src/banking/system/BankingSystem.java
@@ -1,21 +1,21 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package banking.system;
+ */ 
+package banking.system; 
 
 /**
  *
  *
  * @author hp
  */
-public class BankingSystem {
+public class BankingSystem { 
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) { 
+        // TODO code application logic here 
     }
     
 }
