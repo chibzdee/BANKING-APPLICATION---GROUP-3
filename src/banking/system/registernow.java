75a6d4b9c5a431fcc1a71c4112014bd94c898dea
PraiseElom <134976536+PraiseElom@users.noreply.github.com>
2023-05-30 21:27:30 +0100

    Update registernow.java

--- src/banking/system/registernow.java
+++ src/banking/system/registernow.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
+
 package banking.system;
 
 import java.sql.Connection;
